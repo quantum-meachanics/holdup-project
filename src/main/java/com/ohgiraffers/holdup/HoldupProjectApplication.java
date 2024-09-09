a86c9a1a042d03be77428f18ffc9dcb75ff26bd1
yanghyeyeon <yhy4058@gmail.com>
2024-09-09 17:47:03 +0900

    first commit

---
+++ src/main/java/com/ohgiraffers/holdup/HoldupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.holdup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoldupProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoldupProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraffers/holdup/HoldupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.holdup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoldupProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
