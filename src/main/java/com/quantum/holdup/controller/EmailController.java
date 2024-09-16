c48bb719ca68c2fcd2314506d7dfe610dc0c9fd5
wsm\seongmin <wdh970616@naver.com>
2024-09-16 16:59:52 +0900

    url 재설정 및 불필요한 필터 삭제

--- src/main/java/com/quantum/holdup/controller/EmailController.java
+++ src/main/java/com/quantum/holdup/controller/EmailController.java
@@ -2,13 +2,11 @@ package com.quantum.holdup.controller;
 
 import com.quantum.holdup.service.EmailService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/holdup")
 public class EmailController {
 
     private final EmailService emailService;
