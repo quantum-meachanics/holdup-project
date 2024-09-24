a7854713e38aa42a3a043882423cb7cd2936f58f
kingHyen <modan8199@gmail.com>
2024-09-24 10:17:28 +0900

    - fix - login signup 관련 수정 - DTO @Buider 통일 - EmailService Detail 수정 - CreateMemberDTO return 값 수정 - PasswordResetService 삭제 - ResponseMessage @Buider 수정 - MemberDTO @Buider 수정 - SearchMemberEmailDTO @Buider 수정 commit

--- src/main/java/com/quantum/holdup/domain/dto/CreateMemberDTO.java
+++ src/main/java/com/quantum/holdup/domain/dto/CreateMemberDTO.java
@@ -1,17 +1,11 @@
 package com.quantum.holdup.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
+import lombok.*;
 import java.time.LocalDate;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
-@Setter
 public class CreateMemberDTO {
 
     private String email; // 회원 이메일
@@ -20,11 +14,22 @@ public class CreateMemberDTO {
     private String phone; // 회원 휴대전화번호
     private String name; // 회원 본명
     private String address; // 회원 주소
+    private LocalDate birthday; // 회원 생년월일
     @JsonIgnore // 이 필드는 JSON 변환 시 무시됩니다.
     private String addressDetail;
     @JsonIgnore // 이 필드는 JSON 변환 시 무시됩니다.
     private String confirmPassword;
-    private LocalDate birthday; // 회원 생년월일
 
-
+    @Builder
+    public CreateMemberDTO(String email, String password, String nickname, String phone, String name, String address, String addressDetail, String confirmPassword, LocalDate birthday) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.phone = phone;
+        this.name = name;
+        this.address = address;
+        this.addressDetail = addressDetail;
+        this.confirmPassword = confirmPassword;
+        this.birthday = birthday;
+    }
 }
--- src/main/java/com/quantum/holdup/domain/dto/SearchMemberEmailDTO.java
+++ src/main/java/com/quantum/holdup/domain/dto/SearchMemberEmailDTO.java
@@ -1,19 +1,18 @@
 package com.quantum.holdup.domain.dto;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
-@Setter
 public class SearchMemberEmailDTO {
 
     private String name; // 회원 본명
     private String phone; // 회원 휴대전화번호
 
-
+    @Builder
+    public SearchMemberEmailDTO(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
 }
