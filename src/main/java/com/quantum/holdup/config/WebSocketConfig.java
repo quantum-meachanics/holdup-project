4dcf0791b1fbc2bbd624c6b1cf8ef7d8269ab9ad
parkhayarn <s0224y92@gmail.com>
2024-09-25 13:07:12 +0900

    Feature/1대1 메시지 전송 및 수신 패키지 구조 및 코드작성

--- src/main/java/com/quantum/holdup/config/WebSocketConfig.java
+++ src/main/java/com/quantum/holdup/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.quantum.holdup.config;
 
-import com.quantum.holdup.server.ChatWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
