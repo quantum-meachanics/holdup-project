d47d3d73fd8958048e9316c9b6ba8793da92468b
parkhayarn <s0224y92@gmail.com>
2024-09-26 11:31:08 +0900

    Feature/1대1 메시지 전송 및 수신 코드 수정 및 필요없는 임포트 삭제

--- src/main/java/com/quantum/holdup/controller/ChatMessageController.java
+++ src/main/java/com/quantum/holdup/controller/ChatMessageController.java
@@ -23,14 +23,14 @@ public class ChatMessageController {
     // WebSocket으로 메시지 전송 시 호출되는 메서드
     @MessageMapping("/sendMessage/{roomId}")
     @SendTo("/topic/{roomId}")
-    public ChatMessageDTO sendMessage(@DestinationVariable long roomId, @RequestBody ChatMessageDTO messageDTO) {
+    public ChatMessageDTO sendMessage(@RequestBody ChatMessageDTO messageDTO) {
         // ChatMessageService의 saveMessage 메서드를 호출하여 메시지를 저장
         return chatMessageService.saveMessage(messageDTO);
     }
 
     // 특정 채팅방의 메시지 조회
     @GetMapping("/messages/{roomId}")
-    public List<ChatMessageDTO> getMessages(@PathVariable Long roomId) {
+    public List<ChatMessageDTO> getMessages(@PathVariable long roomId) {
         // ChatRoomService에서 getMessagesByRoomId를 호출하여 메시지 목록을 가져옴
         return chatMessageService.getMessagesByRoomId(roomId);
     }
