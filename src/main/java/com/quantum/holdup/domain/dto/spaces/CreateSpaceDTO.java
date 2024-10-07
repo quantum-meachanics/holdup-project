66d1de2f248cf286d826b1ead01fcba37314ddfc
wdh970616 <wdh970616@naver.com>
2024-10-07 17:05:22 +0900

    공간 이용 페이지 표시 정보 추가중

--- src/main/java/com/quantum/holdup/domain/dto/spaces/CreateSpaceDTO.java
+++ src/main/java/com/quantum/holdup/domain/dto/spaces/CreateSpaceDTO.java
@@ -2,8 +2,10 @@ package com.quantum.holdup.domain.dto.spaces;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
