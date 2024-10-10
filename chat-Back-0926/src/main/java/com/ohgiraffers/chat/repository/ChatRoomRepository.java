2cad726771cda7b6e1e304bbf1e5df866a277b38
parkhayarn <s0224y92@gmail.com>
2024-10-10 18:58:12 +0900

    REPOSITORY 업로드예정

--- chat-Back-0926/src/main/java/com/ohgiraffers/chat/repository/ChatRoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ohgiraffers.chat.repository;
-
-import com.ohgiraffers.chat.entity.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-}
--- chat-Back-0926/src/main/resources/mysql/holdupdb.sql
+++
@@ -1,4 +0,0 @@
-drop user quantum;
-CREATE DATABASE IF NOT EXISTS holdupdb;
-CREATE USER IF NOT EXISTS quantum identified by 'quantum';
-GRANT ALL PRIVILEGES ON holdupdb.* to 'quantum'@'%';
